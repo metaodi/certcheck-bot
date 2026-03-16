d362ea4e44870f059b827643c1338a91fdef137b
Stefan Oderbolz <oderbolz@gmail.com>
2026-03-16 10:16:08 +0100

    Increase SSL certificate expiry threshold to 45 days

--- certcheck.py
+++ certcheck.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 
 from github import Github, GithubException
 
-EXPIRY_THRESHOLD_DAYS = 14
+EXPIRY_THRESHOLD_DAYS = 45
 BOT_LABEL_NAME = "ssl-expiry"
 BOT_LABEL_COLOR = "e11d48"
 BOT_LABEL_DESCRIPTION = "SSL/TLS certificate expiry issue"
